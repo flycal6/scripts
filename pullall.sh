acc2ff8d4eecb017886db3c633b882d11a24a516
brian ray thomas <brthomas@blm.gov>
2019-01-04 07:35:06 -0700

    some text formatting with tput;

--- pullall.sh
+++ pullall.sh
@@ -1,76 +1,87 @@
 #!/bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 cd C:\\Users\\brthomas\\workspaces\\git\\afmss-apdx-ws
 wait
+printf "\n***************************\n"
 pwd
 
-printf "\npulling apdx ws\n"
+printf "${bold}\npulling apdx ws\n${normal}"
 git pull
 wait
 
 cd C:\\Users\\brthomas\\workspaces\\git\\afmss-apdx-ui
 wait
+printf "\n***************************\n"
 pwd
 
-printf "\npulling apdx ui\n"
+printf "${bold}\npulling apdx ui\n${normal}"
 git pull
 wait
 
 cd C:\\Users\\brthomas\\workspaces\\git\\afmss-common-ws
 wait
+printf "\n***************************\n"
 pwd
 
-printf "\npulling common ws\n"
+printf "${bold}\npulling common ws\n${normal}"
 git pull
 wait
 
 cd C:\\Users\\brthomas\\workspaces\\git\\afmss-common-ui
 wait
+printf "\n***************************\n"
 pwd
 
-printf "\npulling common ui\n"
+printf "${bold}\npulling common ui\n${normal}"
 
 git pull
 wait
 
 cd C:\\Users\\brthomas\\workspaces\\git\\afmss-correspondence-ws
 wait
+printf "\n***************************\n"
 pwd
 
-printf "\npulling correspondence ws\n"
+printf "${bold}\npulling correspondence ws\n${normal}"
 
 git pull
 wait
 
 cd C:\\Users\\brthomas\\workspaces\\git\\bizflowCommon-ws
 wait
+printf "\n***************************\n"
 pwd
 
-printf "\npulling bizflow common ws\n"
+printf "${bold}\npulling bizflow common ws\n${normal}"
 
 git pull
 wait
 
 cd C:\\Users\\brthomas\\workspaces\\git\\afmss-global-ui
 wait
+printf "\n***************************\n"
 pwd
 
-printf "\npulling global ui\n"
+printf "${bold}\npulling global ui\n${normal}"
 
 git pull
 wait
 
 cd C:\\Users\\brthomas\\workspaces\\git\\afmss-global-ws
 wait
+printf "\n***************************\n"
 pwd
 
-printf "\npulling global ws\n"
+printf "${bold}\npulling global ws\n${normal}"
 
 git pull
 wait
 
 
 
-printf "\n\nFinished Pulling All Repos\n"
+printf "${bold}\n\nFinished Pulling All Repos\n${normal}"
 
 read
