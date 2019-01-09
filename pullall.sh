c6cfd9b99dfe78af3c83c77d3e77853afeb8787a
brian ray thomas <brthomas@blm.gov>
2019-01-09 09:05:28 -0700

    added more afmss projects;

--- pullall.sh
+++ pullall.sh
@@ -4,14 +4,20 @@ red=$(tput setaf 1)
 white=$(tput setaf 7)
 green=$(tput setaf 2)
 
+# add afmss projects to array to be pulled
 declare -a arr=("C:\\Users\\brthomas\\workspaces\\git\\afmss-apdx-ws"
                 "C:\\Users\\brthomas\\workspaces\\git\\afmss-apdx-ui"
                 "C:\\Users\\brthomas\\workspaces\\git\\afmss-common-ws"
                 "C:\\Users\\brthomas\\workspaces\\git\\afmss-common-ui"
                 "C:\\Users\\brthomas\\workspaces\\git\\afmss-correspondence-ws"
                 "C:\\Users\\brthomas\\workspaces\\git\\bizflowCommon-ws"
+                "C:\\Users\\brthomas\\workspaces\\git\\bizflowDev-bizflowCustom"
                 "C:\\Users\\brthomas\\workspaces\\git\\afmss-global-ws"
                 "C:\\Users\\brthomas\\workspaces\\git\\afmss-global-ui"
+                "C:\\Users\\brthomas\\workspaces\\git\\afmss-portal"
+                "C:\\Users\\brthomas\\workspaces\\git\\afmss-portal-ws"
+                "C:\\Users\\brthomas\\workspaces\\git\\wisx-ws"
+                "C:\\Users\\brthomas\\workspaces\\git\\wisx-ui"
                 "C:\\Users\\brthomas\\workspaces\\git\\afmss-sundriesx-ws"
                 "C:\\Users\\brthomas\\workspaces\\git\\afmss-sundriesx-ui"
                 )
