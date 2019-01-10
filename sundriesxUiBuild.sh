404d6a4fa67a203e08ccb75c0227998654dd0604
brian ray thomas <brthomas@blm.gov>
2019-01-10 15:19:32 -0700

    created build script for sundriesx-ui

---
+++ sundriesxUiBuild.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+green=$(tput setaf 2)
+white=$(tput setaf 7)
+
+echo Deleting old build files...
+
+rm ./src/main/webapp/sundriesx-common.js
+rm ./src/main/webapp/sundriesx-core.js
+rm ./src/main/webapp/sundriesx-plugins.min.js
+
+wait
+echo ${green}Finished deleting old build files!${white}
+
+mvn clean package
+
+wait
+echo Copying new build files
+
+cp ./target/afmss-sundriesx-ui/sundriesx-common.js ./src/main/webapp/sundriesx-common.js
+cp ./target/afmss-sundriesx-ui/sundriesx-core.js ./src/main/webapp/sundriesx-core.js
+cp ./target/afmss-sundriesx-ui/sundriesx-plugins.min.js ./src/main/webapp/sundriesx-plugins.min.js
+
+wait
+echo ${green}Finished copying new build files!${white}
+
+read
