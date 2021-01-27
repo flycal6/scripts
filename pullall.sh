0707372f9b604545d227196c55f334f9249b966d
Brian Ray Thomas <brthomas@blm.gov>
2021-01-27 13:08:40 -0800

    comment out currently unused vars; fix some stuff identified by shellcheck: move vars out of printf statements, use -gt or -lt instead of > or <, double quote all vars

--- pullall.sh
+++ pullall.sh
@@ -4,12 +4,12 @@ red=$(tput setaf 1)
 white=$(tput setaf 7)
 green=$(tput setaf 2)
 
-tomcat=../../../tools/apache-tomcat-8.5.39/webapps/
+#tomcat=../../../tools/apache-tomcat-8.5.39/webapps/
 
 failures=()
 deployfailures=()
 updatefailures=()
-deploy=""
+#deploy=""
 
 # add afmss projects to array to be pulled
 declare -a arr=("C:\\Users\\brthomas\\workspaces\\git\\afmss-apdx-ws"
@@ -33,11 +33,11 @@ declare -a arr=("C:\\Users\\brthomas\\workspaces\\git\\afmss-apdx-ws"
 
 for repo in "${arr[@]}"
 do
-    cd "$repo"
+    cd "$repo" || exit
     wait
     branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-    printf "${red}\n***************************\n"
-    printf "pulling:${green} ${PWD##*/} ${white} ($branch)\n"
+    printf "%s\n***************************\n" "${red}"
+    printf "pulling:%s %s %s ($branch)\n" "${green}" "${PWD##*/}" "${white}"
 
     gitStatus=$(git pull 2>&1)
     wait
@@ -49,24 +49,24 @@ do
     then
         if [[ "$repo" != *bizflowCustom ]]
         then
-            printf "${green}\n***************************\n"
-            printf "Performing 'mvn clean package' on:${green} ${PWD##*/} ${white}\n"
+            printf "%s\n***************************\n" "${green}"
+            printf "Performing 'mvn clean package' on:%s %s %s\n" "${green}" "${PWD##*/}" "${white}"
             mavenBuild=$(mvn clean package -DskipTests 2>&1)
             wait
             printf "%s\n" "$mavenBuild"
             
             if [[ "$mavenBuild" == *FAILURE* ]]
             then
-                printf "${red}\n***************************\n"
+                printf "%s\n***************************\n" "${red}"
                 printf "%s" "${PWD##*/}"
                 printf " MAVEN BUILD FAILED - WAR NOT DEPLOYED"
-                printf "\n***************************\n${white}"
-                failures+=(${PWD##*/})
+                printf "\n***************************\n%s" "${white}"
+                failures+=("${PWD##*/}")
 
-            elif [[ "$mavenbuild" != *FAILURE* ]]
+            elif [[ "$mavenBuild" != *FAILURE* ]]
             then
-                printf "${green}\n***************************\n"
-                printf "${green}\nMAVEN BUILD SUCCEEDED\n"
+                printf "%s\n***************************\n" "${green}"
+                printf "%s\nMAVEN BUILD SUCCEEDED\n" "${green}"
                 # printf "Re-Deploying: ${green} ${PWD##*/}.war ${white}\n"
             fi
         
@@ -108,63 +108,63 @@ do
             
     elif [[ "$gitStatus" == *Aborting ]]
         then
-            printf "${red}\n***************************\n"
+            printf "%s\n***************************\n" "${red}"
             printf "%s" "${PWD##*/}"
             printf " FAILED TO UPDATE"
-            printf "\n***************************\n${white}"
-            updatefailures+=(${PWD##*/})
+            printf "\n***************************\n%s" "${white}"
+            updatefailures+=("${PWD##*/}")
 
     fi
 done
 
-printf "${green}\n\nFinished Pulling All Repos\n${white}"
+printf "%s\n\nFinished Pulling All Repos\n%s" "${green}" "${white}" 
 
-if [[ ${#updatefailures[@]} > 0 ]]
+if [[ ${#updatefailures[@]} -gt 0 ]]
 then
-    printf "${red}\n***************************\n"
+    printf "%s\n***************************\n" "${red}"
     for updateFail in "${updatefailures[@]}"
     do
-        printf $updateFail
+        printf "%s" "$updateFail"
         printf " FAILED TO UPDATE\n"
     done
     printf "\n***************************\n"
-elif [[ ${#updatefailures[@]} < 1 ]]
+elif [[ ${#updatefailures[@]} -lt 1 ]]
 then
-    printf "${green}\n***************************\n"
-    printf "${white}No Git Update Failures!"
-    printf "${green}\n***************************\n"
+    printf "%s\n***************************\n" "${green}"
+    printf "%sNo Git Update Failures!" "${white}"
+    printf "%s\n***************************\n" "${green}"
 fi
 
-if [[ ${#failures[@]} > 0 ]]
+if [[ ${#failures[@]} -gt 0 ]]
 then
-    printf "${red}\n***************************\n"
+    printf "%s\n***************************\n" "${red}"
     for buildFail in "${failures[@]}"
     do
-        printf $buildFail
+        printf "%s" "$buildFail"
         printf " FAILED TO MAVEN BUILD\n"
     done
     printf "\n***************************\n"
-elif [[ ${#failures[@]} < 1 ]]
+elif [[ ${#failures[@]} -lt 1 ]]
 then
-    printf "${green}\n***************************\n"
-    printf "${white}No Maven Build Failures!"
-    printf "${green}\n***************************\n"
+    printf "%s\n***************************\n" "${green}"
+    printf "%sNo Maven Build Failures!" "${white}"
+    printf "%s\n***************************\n" "${green}"
 fi
 
-if [[ ${#deployfailures[@]} > 0 ]]
+if [[ ${#deployfailures[@]} -gt 0 ]]
 then
-    printf "${red}\n***************************\n"
+    printf "%s\n***************************\n" "${red}"
     for deployFail in "${deployfailures[@]}"
     do
-        printf $deployFail
+        printf "%s""$deployFail"
         printf " FAILED TO DEPLOY\n"
     done
     printf "\n***************************\n"
-elif [[ ${#deployfailures[@]} < 1 ]]
+elif [[ ${#deployfailures[@]} -lt 1 ]]
 then
-    printf "${green}\n***************************\n"
-    printf "${white}No Deployment Failures!"
-    printf "${green}\n***************************\n"
+    printf "%s\n***************************\n" "${green}"
+    printf "%sNo Deployment Failures!" "${white}"
+    printf "%s\n***************************\n" "${green}"
 fi
 
 # uncomment to wait for input before ending
